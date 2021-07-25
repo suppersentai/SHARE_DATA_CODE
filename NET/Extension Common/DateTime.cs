d8d7fc56f58aec38290f6682f691802e97bbe34d
Chung Toàn <chungtoan94@gmail.com>
2021-07-25 12:22:33 +0700

    Add Time Ago
    Thêm hàng tính time ago

--- NET/Extension Common/DateTime.cs
+++ NET/Extension Common/DateTime.cs
@@ -35,46 +35,48 @@ public void date()
 
          
         }
-public string TimeAgo(DateTime yourDate)
-{
-const int SECOND = 1;
-const int MINUTE = 60 * SECOND;
-const int HOUR = 60 * MINUTE;
-const int DAY = 24 * HOUR;
-const int MONTH = 30 * DAY;
+   public static string TimeAgo(DateTime yourDate)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
 
-var ts = new TimeSpan(DateTime.UtcNow.Ticks - yourDate.Ticks);
-double delta = Math.Abs(ts.TotalSeconds);
+            var ts = new TimeSpan(DateTime.Now.Ticks - yourDate.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
 
-if (delta < 1 * MINUTE)
- return ts.Seconds == 1 ? "1 giây trước " : ts.Seconds + “ giây trước";
+            if (delta < 1 * MINUTE)
+                return ts.Seconds == 1 ? "1 giây trước " : ts.Seconds + " giây trước";
 
-if (delta < 2 * MINUTE)
- return "1 phút trước"”"; 
+            if (delta < 2 * MINUTE)
+                return "1 phút trước";
 
-if (delta < 45 * MINUTE)
-  return ts.Minutes + " phút trước”";
 
-if (delta < 90 * MINUTE)
- return "1 giờ trước";
+            if (delta < 45 * MINUTE)
+                return ts.Minutes + " phút trước”";
 
-if (delta < 24 * HOUR)
-  return ts.Hours + " giờ trước";
+            if (delta < 90 * MINUTE)
+                return "1 giờ trước";
 
-if (delta < 48 * HOUR)
- return "hôm qua"
+            if (delta < 24 * HOUR)
+                return ts.Hours + " giờ trước";
 
-if (delta < 30 * DAY)
- return ts.Days + " ngày trướ";
+            if (delta < 48 * HOUR)
+                return "hôm qua";
 
-if (delta < 12 * MONTH)
-{
-  int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
-  return months <= 1 ? "1 tháng trước" : months + " tháng trước";
-}
-else
-{
-  int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
- return years <= 1 ? "1 năm trước" : years + "năm trước";
-}
-}
+
+            if (delta < 30 * DAY)
+                return ts.Days + " ngày trước";
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "1 tháng trước" : months + " tháng trước";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "1 năm trước" : years + "năm trước";
+            }
+        }

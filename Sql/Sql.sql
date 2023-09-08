7ccaa213171bc4c28dd6f15ca8fdf335e2f22420
Chung Toàn <45030037+suppersentai@users.noreply.github.com>
2023-09-08 14:41:48 +0700

    Update Sql.sql
    --- tìm  giá trị trên all table 2

--- Sql/Sql.sql
+++ Sql/Sql.sql
@@ -1,5 +1,76 @@
 
---- tìm  giá trị trên all table 
+--- tìm  giá trị trên all table 1
+execute SearchAllTables 'DCVFM TB: Tai khoan Quy Huu tri bo sung'
+
+select top 1 * from SMSTemplates where TemplateCode like  'SendOTPLoginToSaleman%'
+
+CREATE PROC SearchAllTables
+(
+    @SearchStr nvarchar(100)
+)
+AS
+BEGIN
+
+-- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
+-- Purpose: To search all columns of all tables for a given search string
+-- Written by: Narayana Vyas Kondreddi
+-- Site: http://vyaskn.tripod.com
+-- Tested on: SQL Server 7.0 and SQL Server 2000
+-- Date modified: 28th July 2002 22:50 GMT
+
+DECLARE @Results TABLE(ColumnName nvarchar(370), ColumnValue nvarchar(3630))
+
+SET NOCOUNT ON
+
+DECLARE @TableName nvarchar(256), @ColumnName nvarchar(128), @SearchStr2 nvarchar(110)
+SET  @TableName = ''
+SET @SearchStr2 = QUOTENAME('%' + @SearchStr + '%','''')
+
+WHILE @TableName IS NOT NULL
+BEGIN
+    SET @ColumnName = ''
+    SET @TableName = 
+    (
+        SELECT MIN(QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME))
+        FROM    INFORMATION_SCHEMA.TABLES
+        WHERE       TABLE_TYPE = 'BASE TABLE'
+            AND QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) > @TableName
+            AND OBJECTPROPERTY(
+                    OBJECT_ID(
+                        QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME)
+                         ), 'IsMSShipped'
+                           ) = 0
+    )
+
+    WHILE (@TableName IS NOT NULL) AND (@ColumnName IS NOT NULL)
+    BEGIN
+        SET @ColumnName =
+        (
+            SELECT MIN(QUOTENAME(COLUMN_NAME))
+            FROM    INFORMATION_SCHEMA.COLUMNS
+            WHERE       TABLE_SCHEMA    = PARSENAME(@TableName, 2)
+                AND TABLE_NAME  = PARSENAME(@TableName, 1)
+                AND DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar')
+                AND QUOTENAME(COLUMN_NAME) > @ColumnName
+        )
+
+        IF @ColumnName IS NOT NULL
+        BEGIN
+            INSERT INTO @Results
+            EXEC
+            (
+                'SELECT ''' + @TableName + '.' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) 
+                FROM ' + @TableName + 
+                ' WHERE ' + @ColumnName + ' LIKE ' + @SearchStr2
+            )
+        END
+    END 
+END
+
+SELECT ColumnName, ColumnValue FROM @Results
+END
+
+--- tìm  giá trị trên all table 2
 DECLARE @SearchText nvarchar(255) = 'Bp70CORL~0Cn7zXPolZd]>hu'
 DECLARE @SearchTableName nvarchar(255) = NULL 
 DECLARE @SearchColumnName nvarchar(255) = NULL 
